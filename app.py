2925b20bd75d4ad73a259f2e603f67c350fc3c88
nghiatr84 <nghiabigtran@gmail.com>
2023-05-13 15:25:33 +0300

    configured documentation link, css style folder

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/documentation.html')
+def documentation():
+   return render_template('documentation.html')
+
 if __name__ == '__main__':
   app.run(debug=True)
