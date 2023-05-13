db9cfd28d702803ad3a641a5bf600345d0bce252
Ailus Apollo <apollo.ailus@aalto.fi>
2023-05-13 17:33:21 +0300

    Placed code in function funk() and added lines to save graphs as files.

--- database-to-graph.py
+++ database-to-graph.py
@@ -5,62 +5,69 @@ import numpy as np
 import pandas as pd
 from shapely.geometry import LineString
 
-# read airports
-airports = pd.read_csv("C:/Users/apoll/Documents/College Schoolwork/Industrial "
-                       "Training/industrial-training-group-6/openflights/data/airports.dat", delimiter=',',
-                       names=['id', 'name', 'city', 'country', 'iata',
-                                                                   'icao', 'lat', 'long', 'altitude', 'timezone',
-                                                                   'dst', 'tz', 'type', 'source'])
 
-fig, ax = plt.subplots(facecolor='#ffb6c1')
-fig.set_size_inches(14, 7)
+# import cartopy.crs as ccrs
 
-ax.scatter(airports['long'], airports['lat'], s=1, alpha=1, edgecolors='none')
-ax.axis('off')
+def funk():
+    # read airports
+    airports = pd.read_csv("C:/Users/apoll/Documents/College Schoolwork/Industrial "
+                           "Training/industrial-training-group-6/openflights/data/airports.dat", delimiter=',',
+                           names=['id', 'name', 'city', 'country', 'iata',
+                                  'icao', 'lat', 'long', 'altitude', 'timezone',
+                                  'dst', 'tz', 'type', 'source'])
 
-plt.show()
+    print(airports)
 
-print(airports)
+    fig, ax = plt.subplots(facecolor='#ffb6c1')
+    fig.set_size_inches(14, 7)
 
-# read routes
+    ax.scatter(airports['long'], airports['lat'], s=1, alpha=1, edgecolors='none')
+    ax.axis('off')
 
-routes = pd.read_csv("C:/Users/apoll/Documents/College Schoolwork/Industrial "
-                     "Training/industrial-training-group-6/openflights/data/routes.dat", delimiter=',',
-                     names=['airline', 'id', 'source_airport', 'source_airport_id',
-                                                               'destination_airport', 'destination_airport_id', 'codeshare',
-                                                               'stops', 'equitment'])
+    fig.savefig('airports_map.png')
+    plt.show()
 
-print(routes)
+    # read routes
 
-# manipulate the data to be DataFrames instead of Linestrings.
-source_airports = airports[['name', 'iata', 'icao', 'lat', 'long']]
-destination_airports = source_airports.copy()
-source_airports.columns = [str(col) + '_source' for col in source_airports.columns]
-destination_airports.columns = [str(col) + '_destination' for col in destination_airports.columns]
+    routes = pd.read_csv("C:/Users/apoll/Documents/College Schoolwork/Industrial "
+                         "Training/industrial-training-group-6/openflights/data/routes.dat", delimiter=',',
+                         names=['airline', 'id', 'source_airport', 'source_airport_id',
+                                'destination_airport', 'destination_airport_id', 'codeshare',
+                                'stops', 'equitment'])
 
-routes = routes[['source_airport', 'destination_airport']]
-routes = pd.merge(routes, source_airports, left_on='source_airport', right_on='iata_source')
-routes = pd.merge(routes, destination_airports, left_on='destination_airport', right_on='iata_destination')
+    print(routes)
 
-print(routes.columns)
+    # manipulate the data to be DataFrames instead of Linestrings.
+    source_airports = airports[['name', 'iata', 'icao', 'lat', 'long']]
+    destination_airports = source_airports.copy()
+    source_airports.columns = [str(col) + '_source' for col in source_airports.columns]
+    destination_airports.columns = [str(col) + '_destination' for col in destination_airports.columns]
 
-geometry = [LineString([[routes.iloc[i]['long_source'], routes.iloc[i]['lat_source']], [routes.iloc[i]['long_destination'], routes.iloc[i]['lat_destination']]]) for i in range(routes.shape[0])]
-routes = gpd.GeoDataFrame(routes, geometry=geometry, crs='EPSG:4326')
-print(routes)
+    routes = routes[['source_airport', 'destination_airport']]
+    routes = pd.merge(routes, source_airports, left_on='source_airport', right_on='iata_source')
+    routes = pd.merge(routes, destination_airports, left_on='destination_airport', right_on='iata_destination')
 
-# Data validation:
-fig = plt.figure(facecolor='black')
-ax = plt.axes()
+    print(routes.columns)
 
-fig.set_size_inches(7, 3.5)
-ax.patch.set_facecolor('black')
+    geometry = [LineString([[routes.iloc[i]['long_source'], routes.iloc[i]['lat_source']],
+                            [routes.iloc[i]['long_destination'], routes.iloc[i]['lat_destination']]]) for i in
+                range(routes.shape[0])]
+    routes = gpd.GeoDataFrame(routes, geometry=geometry, crs='EPSG:4326')
+    print(routes)
 
-routes.plot(ax=ax, color='white', linewidth=0.1)
+    # Data validation:
+    fig = plt.figure(facecolor='black')
+    ax = plt.axes()
 
-plt.setp(ax.spines.values(), color='black')
-plt.setp([ax.get_xticklines(), ax.get_yticklines()], color='black')
+    fig.set_size_inches(7, 3.5)
+    ax.patch.set_facecolor('black')
 
-plt.show()
+    routes.plot(ax=ax, color='white', linewidth=0.1)
 
+    plt.setp(ax.spines.values(), color='black')
+    plt.setp([ax.get_xticklines(), ax.get_yticklines()], color='black')
 
+    fig.savefig('routes_map.png')
+    plt.show()
 
+funk()
