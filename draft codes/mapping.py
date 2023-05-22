ba0fbd15b5216df6961f149784d5276cadb7da88
Hoang Nguyen <nguyen.hoang@aalto.fi>
2023-05-22 22:04:43 +0300

    Replace mapping.py, draw flight routes over a map(routes_filter = routes["source_airport"] == "MSQ" filters by source_airport)

--- draft codes/mapping.py
+++ draft codes/mapping.py
@@ -3,31 +3,46 @@ import geopandas as gpd
 import pandas as pd
 import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
+from shapely.geometry import LineString
 
-#Credits: https://scitools.org.uk/cartopy/docs/v0.15/matplotlib/intro.html
+# Flight data:
+airports = pd.read_csv("static/airports.dat", delimiter=',',
+                           names=['id', 'name', 'city', 'country', 'iata',
+                                  'icao', 'lat', 'long', 'altitude', 'timezone',
+                                  'dst', 'tz', 'type', 'source'])
 
-ax = plt.axes(projection=ccrs.PlateCarree())
-ax.stock_img()                                          #Add the map
+routes = pd.read_csv("static/routes.dat", delimiter=',',
+                         names=['airline', 'id', 'source_airport', 'source_airport_id',
+                                'destination_airport', 'destination_airport_id', 'codeshare',
+                                'stops', 'equitment'])
 
-ny_lon, ny_lat = -75, 43                                #Longitude and latitude of 2 cities
-delhi_lon, delhi_lat = 77.23, 28.61                     #TODO: Get coordinates of airports to plot
+routes_filter = routes["source_airport"] == "MSQ"           #Filter only routes with source_airport == MSQ
+routes = routes[routes_filter]
 
-plt.plot([ny_lon, delhi_lon], [ny_lat, delhi_lat],      #Plot coordinates of airports
-         color='blue', linewidth=2, marker='o',
-         transform=ccrs.Geodetic(),
-         )
+# For getting source and destination airports
+source_airports = airports[['name', 'iata', 'icao', 'lat', 'long']]
+destination_airports = source_airports.copy()
+source_airports.columns = [str(col) + '_source' for col in source_airports.columns]
+destination_airports.columns = [str(col) + '_destination' for col in destination_airports.columns]
 
-plt.plot([ny_lon, delhi_lon], [ny_lat, delhi_lat],
-         color='gray', linestyle='--',
-         transform=ccrs.PlateCarree(),
-         )
+routes = routes[['source_airport', 'destination_airport']]
+routes = pd.merge(routes, source_airports, left_on='source_airport', right_on='iata_source')
+routes = pd.merge(routes, destination_airports, left_on='destination_airport', right_on='iata_destination')
 
-plt.text(ny_lon, ny_lat, 'New York',                    #Plot route
-         horizontalalignment='center',
-         transform=ccrs.Geodetic())
+# For plotting flight path over map
+fig = plt.figure(facecolor='black')
+ax = plt.axes(projection=ccrs.Robinson())
+ax.stock_img()
+fig.set_size_inches(7, 3.5)
 
-plt.text(delhi_lon + 3, delhi_lat - 12, 'Delhi',        #Name of airport
-         horizontalalignment='left',
-         transform=ccrs.Geodetic())
+# Takes a list of latitude and longitude pairs and creates a line that joins those points
+geometry = [LineString([[routes.iloc[i]['long_source'], routes.iloc[i]['lat_source']], [routes.iloc[i]['long_destination'], routes.iloc[i]['lat_destination']]]) for i in range(routes.shape[0])]
+routes = gpd.GeoDataFrame(routes, geometry=geometry, crs='EPSG:4326')
 
-plt.show()                                              #Display plot
+routes.plot(ax=ax, transform=ccrs.Geodetic(), color='red', linewidth=0.5, alpha=1)
+
+plt.setp(ax.spines.values(), color='black')
+plt.setp([ax.get_xticklines(), ax.get_yticklines()], color='black')
+ax.set_ylim(-7000000, 8800000)
+
+plt.show()
